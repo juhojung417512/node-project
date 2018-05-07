7290107430a0c86b94a30bae8b2fb8f3384f1199
jeong juho <jooho2479@gmail.com>
2018-05-07 20:20:43 +0900

    - mysql commit

--- chart-test/app.js
+++ chart-test/app.js
@@ -1,12 +1,32 @@
 var app = require("express")();
 var path = require("path");
+var mysql = requrie("mysql");
 var MAX = 100;
+
 app.set("views",path.resolve(__dirname,"views"))
 app.set("view engine","ejs");
 
 app.get("/",function(req,res){
     res.render("index");
 });
+
+//db
+var connection = mysql.createConnection({
+    host: "54.250.2.129",
+    user: "ubuntu",
+    password: "ubuntu",
+    port: 3306,
+    database: "test_db"
+});
+connection.connect();
+connection.query("select * from user",function(err,rows,fields){
+    if(!err){
+        console.log("id : "+rows[id]+ ", name : " + rows[name] + ", date : " + rows[date]);
+    } else {
+        console.log("db connect error!");
+    }
+});
+connection.end();
 
 var server = require("http").createServer(app);
 var io = require("socket.io")(server);
@@ -16,7 +36,6 @@ io.on("connection",function(socket){
         socket.emit("random-value",{
             value: randomInteger()
         });
-        console.log("random value emit!");        
     });        
 });
 
@@ -24,6 +43,8 @@ server.listen(3000,function(){
     console.log("app started on port 3000");
 })
 
+
+//random func
 function randomInteger(){    
     return Math.floor((Math.random()*MAX));
 }
