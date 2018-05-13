02f402aaba631b1898283d4506d7642fdd7af68a
juhojung <juhojung@innospark.com>
2018-05-13 21:58:40 +0900

    react test start!

---
+++ notice_board/react-test/src/Counter.js
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+
+class Counter extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            number : 0
+        }
+        console.log("constructor");
+    }
+
+    handleIncrease = () => {
+        this.setState({
+            number : this.state.number + 1
+        });
+    }
+
+    handleDecrease = () => {
+        this.setState({
+            number: this.state.number - 1
+        });
+    }
+    render(){
+        return(
+            <div>
+                <h1>카운터</h1>
+                <div>값 : {this.state.number}</div>
+                <button onClick={this.handleIncrease}>+</button>
+                <button onClick={this.handleDecrease}>-</button>
+            </div>
+        )
+    }
+}
+
+export default Counter;
