0c53cb93ec0c88560445e196aada602bf42c073b
jeong juho <jooho2479@gmail.com>
2018-06-01 14:29:05 +0900

    asdasd

--- react-native-proj/SendBird/index.js
+++ react-native-proj/SendBird/index.js
@@ -1,5 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-import Main from './src/main';
 
-AppRegistry.registerComponent('SendBird', () => Main);
+AppRegistry.registerComponent('SendBird', () => App);
